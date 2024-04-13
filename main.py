bb37dfe2b8486019c801640fd46203911dfa50fd
stevie <122219518+avocado-beans@users.noreply.github.com>
2024-04-13 18:35:20 +0300

    added form functionality to contact route

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def index():
        # this is the email of the recipient of the message form
        # currently my email but you can change it to yours to get the from message
        # check your spam, that's where i found the messages at first
-       # tell bruk he goofy lol
        recipient = "orthodoxrabbibillclinton42@gmail.com"
        send_email(recipient, msg)
        print("kace.yokohama@gmail.com", email, phone_number, message)
@@ -31,6 +30,16 @@ def about():
     
 @app.route('/contact', methods =["GET", "POST"])
 def contact():
+    if request.method == "POST":
+       full_name = request.form.get("full_name")
+       email = request.form.get("email")
+       phone_number = request.form.get("phone_number") 
+       message = request.form.get("message")
+       msg = "Full Name "+full_name+"\n"+"Email "+email+"\n"+"Phone Number "+phone_number+"\n"+"Message "+message
+
+       recipient = "orthodoxrabbibillclinton42@gmail.com"
+       send_email(recipient, msg)
+       print("kace.yokohama@gmail.com", email, phone_number, message)
     return render_template("contact.html")
     
 @app.route('/service', methods =["GET", "POST"])
